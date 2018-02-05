c1f47c72ee894244c743783a882a2a34097f86d5
Guillaume Baudhuin <g.baudhuin@peoleo.fr>
2018-02-05 18:09:12 +0100

    -

--- tech.js
+++ tech.js
@@ -59,10 +59,14 @@ Tech.getReqOptions = function (url, options) {
         rejectUnauthorized: false,
         requestCert: true,
         agent: false,
+        jar: true, // gère les cookies
         headers: {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept': 'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Connection': 'keep-alive',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36'
+            'Cache-Control': 'max-age=0',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.7',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36'
         }
     };
     if (options) {
